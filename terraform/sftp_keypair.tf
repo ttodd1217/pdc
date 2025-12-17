2b33e8c7ef6dc0ab9649a003cafffdbc88622f77
Timothy Todd <ttodd1217@gmail.com>
2025-12-17 12:43:26 -0600

    fix error

--- terraform/sftp_keypair.tf
+++ terraform/sftp_keypair.tf
@@ -1,5 +1,5 @@
-# Use existing public key from pdc-sftp-server-key.pub
+# Use existing public key - hardcoded
 resource "aws_key_pair" "sftp" {
   key_name   = var.sftp_key_name
-  public_key = file("${path.module}/pdc-sftp-server-key.pub")
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRmkF13Mwt/iq+RecnzBgdRkkFYw7QJGOYAD24BfLNz administrator@DESKTOP-4517OPL"
 }
